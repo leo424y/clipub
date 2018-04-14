a9164bcc1539c38c247bea685fdd4e497f7be968
leo424y <leo424y@gmail.com>
2018-04-14 15:46:05 +0800

    ignore

--- clipub.sh
+++ clipub.sh
@@ -1,13 +1,12 @@
-# TODO in bash
-# watch -n5 'zsh -c "source /Users/y/Documents/Github/clipub/clipub.sh; f"'
+# watch -n5 'bash -c "source ./clipub.sh; f"'
 
-USER_NAME=leo424y # use leo424y it if you want to join clipub project
+USER_NAME=leo424y
 REPO_NAME=clipub
 NOTE_PATH=~/Documents/GitHub/$REPO_NAME
 NOW=$(date +"%Y%m%d-%R:%S")
 
 f(){
-  if [ "$(pbpaste)" = "$(cat temp)" ]; then
+  if [ "$(pbpaste)" = "$(cat .temp)" ]; then
     return
   else
     CONTENT="$(pbpaste)"
@@ -15,6 +14,6 @@ f(){
 
   cd $NOTE_PATH || exit
   git checkout -B $USER_NAME
-  echo "$CONTENT" > "temp"
+  echo "$CONTENT" > ".temp"
   git commit -m "$NOW" -m "$CONTENT" --allow-empty
 }
